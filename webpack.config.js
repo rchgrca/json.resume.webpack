479cdcfcfeb57b4a72986cd4ccc776b8af36c0ec
Rich Garcia <rchgrca@yahoo.com>
2016-12-03 20:05:56 -0800

    adds webpack plugin to remove react minified warning

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ var config = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production')
+          }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false // https://github.com/webpack/webpack/issues/1496
