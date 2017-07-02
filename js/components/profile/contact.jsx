3e7ea2239fdf964c9b7408ad38922e8421c922e8
Rich Garcia <rchgrca@yahoo.com>
2017-07-01 17:57:57 -0700

    adds functions to contact.jsx, centers details icons in profile

--- js/components/profile/contact.jsx
+++ js/components/profile/contact.jsx
@@ -6,25 +6,24 @@ export default class FloatingNav extends Component {
         let { model } = this.props;
         return (
             <div className="contact-details clearfix" id="contact">
-                {model.map((o,i) => {
-                    var fonticon = "fa fa-lg fa-" + o.icon;
-                    return (
-                        <div className="detail" key={i}>
-                            <span className="icon"><i className={fonticon}></i></span>
-                            <span className="info">
-                            {(() => {
-                                if (o.href){
-                                    return <a href={o.href} target="_blank">{o.detail}</a>
-                                } else {
-                                    return o.detail
-                                }
-                            })()}
-
-                            </span>
-                        </div>
-                    )
-                })}
+                {this.getDetails(model)}
             </div>
         )
+    }
+
+    getDetails(model){
+        return model.map((o,i) => {
+            var fonticon = `fa fa-lg fa-${o.icon}`
+            return (
+                <div className="detail" key={i}>
+                    <span className="icon"><i className={fonticon}></i></span>
+                    <span className="info">{this.getDetail(o)}</span>
+                </div>
+            )
+       })
+    }
+
+    getDetail(o){
+        return o.href ? <a href={o.href} target="_blank">{o.detail}</a> : o.detail
     }
 }
